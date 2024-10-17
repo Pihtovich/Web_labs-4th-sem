1a87a15d09a2d41bd7d674d674253fdb509c8217
Nikita Vtiurin <86502038+Pihtovich@users.noreply.github.com>
2024-10-17 23:54:24 +0200

    [task 3]multiplication and division completed

--- lab-02/code/index.php
+++ lab-02/code/index.php
@@ -1,3 +1,4 @@
+
 <?php
 // task #1
 /* Imagine a lot of code here */
@@ -10,3 +11,31 @@ $order .= ', 1 double hemberger AND one XL coke plz'; // concatenation
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name."; // output
+
+// task #2
+$num = 1738; //int
+echo "<br>"; // == "\n"
+echo $num;
+
+echo "<br>"; // "\n"
+
+$num_but_float = 13.337; // float
+echo $num_but_float;
+
+echo "<br>";
+echo $num_but_float - 1.337; // print twelve
+
+echo "<br>";
+$last_month = 1178.23; // difference
+$this_month = 1089.98;
+echo "Разница в расходах: ".$last_month - $this_month;
+
+// task #11
+$num_languages = 4; //vars
+$months = 11;
+
+$days = $months * 16; //actual days
+
+$days_per_language = $days / $num_languages; //avg days to 1 lang
+
+echo $days_per_language; // print
